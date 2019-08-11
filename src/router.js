fcaa73a078fac8058c691824b488ce5d3064e5cc
Jason Brake <jasbrake@gmail.com>
2019-08-10 21:48:01 -0400

    Started using Vuex and router

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Servers from './views/Servers.vue'
+import Server from './views/Server.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -12,14 +14,9 @@ export default new Router({
       component: Servers
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () {
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+      path: '/server/:id',
+      name: 'server',
+      component: Server
     }
   ]
 })
---
+++ src/views/Server.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <section class="section">
+      <div class="container is-full">
+        <span>
+          <b-button icon-left="arrow-left" @click="$router.go(-1)">
+            Back
+          </b-button>
+          <h1 class="title title-text is-inline">{{ server.description }}</h1>
+        </span>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'serverDetail',
+  computed: {
+    server () {
+      return this.$store.getters.serverByKey(this.$route.params.id) || {}
+    }
+  },
+  methods: {
+  },
+  created () {
+  },
+  beforeDestroy () {
+  }
+}
+</script>
+
+<style scoped>
+td:hover {
+  cursor: pointer;
+}
+.title-text {
+  margin-left: 1rem;
+}
+</style>
