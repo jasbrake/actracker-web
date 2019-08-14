a9a42fb73b4b6a74e3ac949f886be4b3e9a6b08c
Jason Brake <jasbrake@gmail.com>
2019-08-13 21:54:10 -0400

    Added search page and setup router (#1)

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Servers from './views/Servers.vue'
 import Server from './views/Server.vue'
+import Search from './views/Search.vue'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/server/:id',
       name: 'server',
       component: Server
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
     }
   ]
 })
