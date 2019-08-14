ca7cabe60ace6b21b6ae1d9b478eeb5e219e682c
Jason Brake <jasbrake@gmail.com>
2019-08-13 21:42:51 -0400

    Added search page and setup router

---
+++ src/views/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <div class="container is-full">
+      <h1 class="title">Search</h1>
+      <section>
+        <b-field>
+          <b-radio-button v-model="searchType" native-value="player">
+            <span>Player</span>
+          </b-radio-button>
+          <b-radio-button v-model="searchType" native-value="game">
+            <span>Game</span>
+          </b-radio-button>
+        </b-field>
+        <b-field>
+          <b-input label="Search">
+          </b-input>
+        </b-field>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchType: 'player'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
